e5a561d25f7e390e609da17a5cbd5348a2237b99
Abhishek kumar <abhishekgolu625@gmail.com>
2025-03-19 22:03:05 +0530

    Update Apps.java

--- Java-main/src/Oops/Apps.java
+++ Java-main/src/Oops/Apps.java
@@ -27,7 +27,7 @@ class Insta extends softwares{
     @Override
     void app_open() {
         while (true){
-            if (user_name.equals("Akbar") && password.equals("123akbar")){
+            if (user_name.equals("Abhi") && password.equals("abhi")){
                 System.out.println("Successfully Login,App Is Open...");
                 break;
             }
